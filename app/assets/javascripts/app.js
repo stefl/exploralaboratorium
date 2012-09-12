6c551986750ecc60c074e2d660c40e5edd9eb3bd
Stef Lewandowski <stef@stef.io>
2012-09-12 15:34:21 +0100

    deck

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,14 +1,6 @@
 $(function() {
   $(document).foundationNavigation();
   $.waypoints.settings.scrollThrottle = 30;
-  $('#wrapper').waypoint(function(event, direction) {
-    //$('.top').toggleClass('hidden', direction === "up");
-  }, {
-    offset: '-100%'
-  }).find('#main_nav').waypoint(function(event, direction) {
-    $(this).parent().toggleClass('sticky', direction === "down");
-    event.stopPropagation();
-  });
 
   $('section').waypoint(function(event, direction) {
    console.log($(this).attr("id"));
