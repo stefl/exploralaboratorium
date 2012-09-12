61041b7cbffdb0887ce9cabfe6b416c1c0543d33
Stef Lewandowski <stef@stef.io>
2012-09-12 16:07:01 +0100

    more

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -7,12 +7,16 @@ $(function() {
 
    $("#main_nav li").removeClass();
    $("#main_nav li#section_" + $(this).attr("id")).addClass("active");
+  }, {
+   offset: 'bottom-in-view'
   });
 
-  $("#main_nav li a").click(function(e) {
+  $("#home-page #main_nav li a").click(function(e) {
     $("#main_nav li").removeClass();
     $(this).parent().addClass("active");
-    $($(this).attr("href").split("/")[1]).scrollTo();
+    var selector = $(this).attr("href").split("/")[1];
+    console.log(selector);
+    $(selector).ScrollTo();
     e.preventDefault();
   });
 
