0e3b5ea66a300e7b434a9c603be3b20d38a336f4
Stef Lewandowski <stef@stef.io>
2012-09-14 11:29:26 +0100

    remove console

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,7 +3,6 @@ $(function() {
   $.waypoints.settings.scrollThrottle = 30;
 
   $('section').waypoint(function(event, direction) {
-   console.log($(this).attr("id"));
 
    $("#main_nav li").removeClass();
    $("#main_nav li#section_" + $(this).attr("id")).addClass("active");
@@ -15,7 +14,6 @@ $(function() {
     $("#main_nav li").removeClass();
     $(this).parent().addClass("active");
     var selector = $(this).attr("href").split("/")[1];
-    console.log(selector);
     $(selector).ScrollTo();
     e.preventDefault();
   });
