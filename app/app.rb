18d5ddd0a29c73723dddb90c100033d52dd59ea6
Stef Lewandowski <stef@stef.io>
2012-09-14 11:47:17 +0100

    inline numbers

--- app/app.rb
+++ app/app.rb
@@ -10,8 +10,9 @@ class Explora < Padrino::Application
   #enable :sessions
 
   set :cache, Padrino::Cache::Store::Memcache.new(::Dalli::Client.new(ENV["MEMCACHE_SERVERS"] || '127.0.0.1:11211', :exception_retry_limit => 1))
-  enable :caching
   
+  enable :caching if Padrino.env == :production
+
   set :google_analytics, "UA-34825962-1"
   set :typekit, "rvb1ljc"
   set :title, "The Exploralaboratorium"
