7f45efafb6f023352131b45fa47f7b2404e29a28
Stef Lewandowski <stef@stef.io>
2012-09-14 11:17:12 +0100

    caching

--- app/app.rb
+++ app/app.rb
@@ -6,7 +6,7 @@ class Explora < Padrino::Application
 
   sprockets :minify => (Padrino.env == :production)
 
-  enable :sessions
+  #enable :sessions
 
   set :cache, Padrino::Cache::Store::Memcache.new(::Dalli::Client.new(ENV["MEMCACHE_SERVERS"] || '127.0.0.1:11211', :exception_retry_limit => 1))
 
--- app/controllers.rb
+++ app/controllers.rb
@@ -1,8 +1,10 @@
 Explora.controllers  do
-  get :index, :map => "/" do
+  get :index, :map => "/", :cache => true do
+    expires_in 300
     render :"pages/home"
   end
-  get :faq, :map => "/faq" do
+  get :faq, :map => "/faq", :cache => true do
+    expires_in 300
     render :"pages/faq"
   end
   post :create_idea, :map => "/ideas" do
